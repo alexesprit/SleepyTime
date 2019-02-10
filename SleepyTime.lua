946f56d997749a5f454ee8b97fbb844c710cc0fd
alexesprit <alex.esprit@gmail.com>
2019-02-10 16:34:29 +0300

    Update meter style

--- SleepyTime.lua
+++ SleepyTime.lua
@@ -1,9 +1,8 @@
-local MESSAGE =
+local MESSAGE_1 =
 'It takes the average human %d minutes to fall\
 asleep. If you head to bed right now, you should\
-try to wake up at one of the following times:\
-%s. \
-A good night\'s sleep consists of 5-6 sleep cycles.'
+try to wake up at one of the following times:'
+local MESSAGE_2 = 'A good night\'s sleep consists of 5-6 sleep cycles.'
 
 local CYCLES_ENDS_COUNT = 6
 local CYCLE_DURATION = 90
@@ -14,8 +13,8 @@ local CYCLE_DURATION_VAR = 'CycleDuration'
 local SIMPLE_MODE_VAR = 'SimpleMode'
 
 local function calculateCyclesEnds(fallAsleepDuration, cycleDuration)
-    cyclesEndsArray = {0, 0, 0, 0, 0, 0}
-    fallAsleepTime = os.time() + fallAsleepDuration * 60
+    local cyclesEndsArray = {0, 0, 0, 0, 0, 0}
+    local fallAsleepTime = os.time() + fallAsleepDuration * 60
 
     for i = 1, CYCLES_ENDS_COUNT do
         wakeUpTime = fallAsleepTime + i * cycleDuration * 60
@@ -30,20 +29,23 @@ local function setStringMeterText(meterName, text)
 end
 
 function Update()
-    fallAsleepDuration = SKIN:GetVariable(
+    local fallAsleepDuration = SKIN:GetVariable(
         FALL_ASLEEP_DURATION_VAR,
         FALL_ASLEEP_DURATION
     )
-    cycleDuration = SKIN:GetVariable(CYCLE_DURATION_VAR, CYCLE_DURATION)
+    local cycleDuration = SKIN:GetVariable(CYCLE_DURATION_VAR, CYCLE_DURATION)
 
-    cyclesEndsArray = calculateCyclesEnds(fallAsleepDuration, cycleDuration)
-    cyclesEndsText = table.concat(cyclesEndsArray, ' or ')
+    local cyclesEndsArray = calculateCyclesEnds(fallAsleepDuration, cycleDuration)
+    local cyclesEndsText = table.concat(cyclesEndsArray, ' or ')
 
-    simpleMode = SKIN:GetVariable(SIMPLE_MODE_VAR, 'False')
-    if simpleMode == 'False' then
-        messageText = string.format(MESSAGE, fallAsleepDuration, cyclesEndsText)
-        setStringMeterText("MainString", messageText)
-    elseif simpleMode == 'True' then
-        setStringMeterText("MainString", cyclesEndsText)
+    local isSimpleMode = SKIN:GetVariable(SIMPLE_MODE_VAR, 'False')
+    if isSimpleMode == 'False' then
+        setStringMeterText("Desc1String", string.format(MESSAGE_1, fallAsleepDuration))
+        setStringMeterText("Desc2String", MESSAGE_2)
+    elseif isSimpleMode == 'True' then
+        setStringMeterText("Desc1String", "")
+        setStringMeterText("Desc2String", "")
     end
+
+    setStringMeterText("MainString", cyclesEndsText)
 end
